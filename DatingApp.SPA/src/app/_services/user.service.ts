d25b0a0bf4b39b9ba1503d007bcac93b4b4d7b3a
Salvatore <salvatore.m@gmail.com>
2019-03-24 22:11:08 +0100

    Minor bugfix environment + added getUsers() mlc

--- DatingApp.SPA/src/app/_services/user.service.ts
+++ DatingApp.SPA/src/app/_services/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Iuser } from '../_models/iuser';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
--- DatingApp.SPA/src/app/member-list/member-list.component.ts
+++ DatingApp.SPA/src/app/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Iuser } from '../_models/iuser';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  users: Iuser[];
 
-  constructor() { }
+  constructor(private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((users: Iuser[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
