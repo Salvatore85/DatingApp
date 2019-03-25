ab1c380fbe3f6fac10528e9590e22f96c4b4c287
Salvatore <salvatore.m@gmail.com>
2019-03-25 21:38:08 +0100

    Moved member-list to members folder

--- DatingApp.SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp.SPA/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../_services/user.service';
-import { AlertifyService } from '../_services/alertify.service';
-import { Iuser } from '../_models/iuser';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { Iuser } from '../../_models/iuser';
 
 @Component({
   selector: 'app-member-list',
