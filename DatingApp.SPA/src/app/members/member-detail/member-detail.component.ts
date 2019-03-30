55f31054e362f2090bc665b065a53dcced8b9af2
Salvatore <salvatore.m@gmail.com>
2019-03-30 20:15:19 +0100

    Added memberdetail component

---
+++ DatingApp.SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Iuser } from '../../_models/iuser';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: Iuser;
+
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: Iuser) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
