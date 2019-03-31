6933720c76a617e0209409f90708cf10e4c7c7cc
Salvatore <salvatore.m@gmail.com>
2019-03-31 13:29:36 +0200

    Add member edit comp and member edit resolver

---
+++ DatingApp.SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Iuser } from 'src/app/_models/iuser';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  user: Iuser;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+}
